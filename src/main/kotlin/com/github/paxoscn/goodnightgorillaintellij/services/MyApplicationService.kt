30364a78071a55c456fa5adaa568e1c889709599
GitHub Action <action@github.com>
2020-12-04 13:17:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/paxoscn/goodnightgorillaintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.paxoscn.goodnightgorillaintellij.services
+
+import com.github.paxoscn.goodnightgorillaintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/paxoscn/goodnightgorillaintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paxoscn/goodnightgorillaintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.paxoscn.goodnightgorillaintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.paxoscn.goodnightgorillaintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
